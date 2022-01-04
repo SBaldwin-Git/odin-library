c92a6c768bd12e52242ed2be2d155ba3f198b018
Sam Baldwin <sambaldwin92@gmail.com>
2022-01-04 14:08:20 +0000

    Started JS card class

--- script.js
+++ script.js
@@ -10,6 +10,42 @@ class Book {
     }
 }
 
+class Card{
+    constructor(title, author, pages, read){
+        //Book info
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+
+        //Card div
+        let cardDiv = document.createElement('div');
+        cardDiv.className = 'card';
+
+        //Headers
+        let titleHeader = document.createElement('h1');
+        titleHeader.innerText('Title:');
+        let authorHeader = document.createElement('h2');
+        authorHeader.innerText('Author:');
+        let pagesHeader = document.createElement('h2');
+        pagesHeader.innerText('Pages:');
+        let readHeader = document.createElement('h2');
+        readHeader.innerText('Read/Unread:');
+
+        //Book info HTML
+        let bookTitle = document.createElement('p').innerHTML(title);
+        let bookAuthor = document.createElement('p').innerHTML(author);
+        let bookPages = document.createElement('p').innerHTML(pages);
+        let bookIsRead;
+
+        if(read == true){
+            bookIsRead = document.createElement('p').innerHTML('Finished');
+        }   else{
+            bookIsRead = document.createElement('p').innerHTML('Unfinished');
+        }
+    }
+}
+
 // Test books
 let book1 = new Book('The Fellowship of the Ring', 'J.R.R Tolkien', 300, true);
 let book2 = new Book('The Two Towers', 'J.R.R Tolkien', 400, true);
