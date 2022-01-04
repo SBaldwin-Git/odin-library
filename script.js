aa52bd1942215d77a751f0edfd4b2824277392cb
Sam Baldwin <sambaldwin92@gmail.com>
2022-01-04 14:47:08 +0000

    Cards now created from library array

--- script.js
+++ script.js
@@ -5,58 +5,78 @@ class Book {
         this.pages = pages;
         this.read = read;
     }
+
+    getT
+
     getInfo() {
         return this.title + ',' + this.author + ',' + this.pages + ',' + this.read;
     }
 }
 
-class Card{
-    constructor(title, author, pages, read){
-        //Book info
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.read = read;
+function createCard(title, author, pages, read) {
 
-        //Card div
-        let cardDiv = document.createElement('div');
-        cardDiv.className = 'card';
+    //Card div
+    let cardDiv = document.createElement('div');
+    cardDiv.className = 'card';
 
-        //Headers
-        let titleHeader = document.createElement('h1');
-        titleHeader.innerText('Title:');
-        let authorHeader = document.createElement('h2');
-        authorHeader.innerText('Author:');
-        let pagesHeader = document.createElement('h2');
-        pagesHeader.innerText('Pages:');
-        let readHeader = document.createElement('h2');
-        readHeader.innerText('Read/Unread:');
+    //Headers
+    let titleHeader = document.createElement('h1');
+    titleHeader.textContent = 'Title:';
+    let authorHeader = document.createElement('h2');
+    authorHeader.textContent = 'Author:';
+    let pagesHeader = document.createElement('h2');
+    pagesHeader.textContent = 'Pages:';
+    let readHeader = document.createElement('h2');
+    readHeader.textContent = 'Finished / Unfinished:';
 
-        //Book info HTML
-        let bookTitle = document.createElement('p').innerHTML(title);
-        let bookAuthor = document.createElement('p').innerHTML(author);
-        let bookPages = document.createElement('p').innerHTML(pages);
-        let bookIsRead;
+    //Book info HTML
+    let bookTitle = document.createElement('p');
+    bookTitle.textContent = title;
+    let bookAuthor = document.createElement('p');
+    bookAuthor.textContent = author;
+    let bookPages = document.createElement('p');
+    bookPages.textContent = pages;
+    let bookIsRead = document.createElement('p');
 
-        if(read == true){
-            bookIsRead = document.createElement('p').innerHTML('Finished');
-        }   else{
-            bookIsRead = document.createElement('p').innerHTML('Unfinished');
-        }
+    if (read == true) {
+        bookIsRead.textContent = 'Finished';
+    } else {
+        bookIsRead.textContent = 'Unfinished';
     }
+
+    //Add all elements to card
+    cardDiv.appendChild(titleHeader);
+    cardDiv.appendChild(bookTitle);
+    cardDiv.appendChild(authorHeader);
+    cardDiv.appendChild(bookAuthor);
+    cardDiv.appendChild(pagesHeader);
+    cardDiv.appendChild(bookPages);
+    cardDiv.appendChild(readHeader);
+    cardDiv.appendChild(bookIsRead);
+    //Add card to body
+    body.appendChild(cardDiv);
 }
+
+//HTML body
+let body = document.querySelector('#cardContainer');
 
 // Test books
 let book1 = new Book('The Fellowship of the Ring', 'J.R.R Tolkien', 300, true);
 let book2 = new Book('The Two Towers', 'J.R.R Tolkien', 400, true);
 let book3 = new Book('The Return of the King', 'J.R.R Tolkien', 500, false);
+let book4 = new Book('The Return of the Ting', 'J.R.R Tolkien', 500, false);
+let book5 = new Book('The Return of the Bling', 'J.R.R Tolkien', 500, false);
 
 //Library array holding test books for debugging
-let library = [book1, book2, book3];
+let library = [book1, book2, book3, book4, book5];
 
 // Adds new book object from text input to library array
 function addBookToLibrary(title, author, pages, read) {
     library.push(new Book(title, author, pages, read))
 }
+
+library.forEach(book => {
+    createCard(book.title, book.author, book.pages, book.read)
+});
 
 console.log(library[0].getInfo());
