154428338824875105981675413954f476d6b878
Sam Baldwin <sambaldwin92@gmail.com>
2022-01-05 11:46:04 +0000

    Added functioning delete button

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 class Book {
-    constructor(title, author, pages, read) {
+    constructor(title, author, pages, read, bookId) {
         this.title = title;
         this.author = author;
         this.pages = pages;
         this.read = read;
+        this.bookId = bookId;
     }
 }
 
-function createCard(title, author, pages, read) {
+function createCard(title, author, pages, read, bookId) {
 
     //Card div
     let cardDiv = document.createElement('div');
     cardDiv.className = 'card';
+    cardDiv.id = 'bookNum'+bookId;
+    let cardId = bookId;
 
     //Headers
     let titleHeader = document.createElement('h1');
@@ -21,7 +24,7 @@ function createCard(title, author, pages, read) {
     let pagesHeader = document.createElement('h2');
     pagesHeader.textContent = 'Pages:';
     let readHeader = document.createElement('h2');
-    readHeader.textContent = 'Finished / Unfinished:';
+    readHeader.textContent = 'Status:';
 
     //Book info HTML
     let bookTitle = document.createElement('p');
@@ -38,7 +41,7 @@ function createCard(title, author, pages, read) {
         bookIsRead.textContent = 'Unfinished';
     }
 
-    //Add all elements to card
+    //Add all text elements to card
     cardDiv.appendChild(titleHeader);
     cardDiv.appendChild(bookTitle);
     cardDiv.appendChild(authorHeader);
@@ -47,7 +50,28 @@ function createCard(title, author, pages, read) {
     cardDiv.appendChild(bookPages);
     cardDiv.appendChild(readHeader);
     cardDiv.appendChild(bookIsRead);
+
+    //Buttons
+    let buttonDiv = document.createElement('div');
+    buttonDiv.className = 'container';
+    let statusButton = document.createElement('button');
+    statusButton.textContent = 'UPDATE';
+    statusButton.id = 'statusBtn';
+    let deleteButton = document.createElement('button');
+    deleteButton.textContent = 'DELETE';
+    deleteButton.id = 'deleteBtn';
+
+    //Delete card and splice library array
+    deleteButton.addEventListener('click', function(){
+        cardDiv.parentNode.removeChild(cardDiv)
+        library.splice(cardId, 1);
+    });
+
+
     //Add card to body
+    buttonDiv.appendChild(statusButton);
+    buttonDiv.appendChild(deleteButton);
+    cardDiv.appendChild(buttonDiv)
     body.appendChild(cardDiv);
 }
 
@@ -55,11 +79,11 @@ function createCard(title, author, pages, read) {
 let body = document.querySelector('#cardContainer');
 
 // Test books
-let book1 = new Book('The Fellowship of the Ring', 'J.R.R Tolkien', 300, true);
-let book2 = new Book('The Two Towers', 'J.R.R Tolkien', 400, true);
-let book3 = new Book('The Return of the King', 'J.R.R Tolkien', 500, false);
-let book4 = new Book('The Return of the Ting', 'J.R.R Tolkien', 500, false);
-let book5 = new Book('The Return of the Bling', 'J.R.R Tolkien', 500, false);
+let book1 = new Book('The Fellowship of the Ring', 'J.R.R Tolkien', 300, true, 0);
+let book2 = new Book('The Two Towers', 'J.R.R Tolkien', 400, true, 1);
+let book3 = new Book('The Return of the King', 'J.R.R Tolkien', 500, false, 2);
+let book4 = new Book('The Return of the Ting', 'J.R.R Tolkien', 500, false, 3);
+let book5 = new Book('The Return of the Bling', 'J.R.R Tolkien', 500, false, 4);
 
 //Library array holding test books for debugging
 let library = [book1, book2, book3, book4, book5];
@@ -70,5 +94,5 @@ function addBookToLibrary(title, author, pages, read) {
 }
 
 library.forEach(book => {
-    createCard(book.title, book.author, book.pages, book.read)
+    createCard(book.title, book.author, book.pages, book.read, this.bookId)
 });
