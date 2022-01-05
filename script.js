eb97f5e6b5d1b5bb100e8666ff735b89da67c12a
Sam Baldwin <sambaldwin92@gmail.com>
2022-01-05 14:41:51 +0000

    Cleaned CSS, checkbox changes correct book

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let table = document.getElementById('libraryTable');
+
 class Book {
     constructor(title, author, pages, read, bookId) {
         this.title = title;
@@ -8,75 +10,58 @@ class Book {
     }
 }
 
-function createCard(title, author, pages, read, bookId) {
+function createRow(title, author, pages, read, bookId) {
 
-    //Card div
-    let cardDiv = document.createElement('div');
-    cardDiv.className = 'card';
-    //
-    // ID CREATOR TEST PLEASE DELETE AFTER
-    cardDiv.id = 'bookNum' + bookId;
-    // ID CREATOR TEST PLEASE DELETE AFTER
-    //
-    let cardId = bookId;
+    //Define cells
+    let row = table.insertRow(1);
+    let titleCell = row.insertCell(0);
+    let authorCell = row.insertCell(1);
+    let pagesCell = row.insertCell(2);
+    let statusCell = row.insertCell(3);
+    let removeButtonCell = row.insertCell(4);
+    let rowId = bookId;
 
-    //Headers
-    let titleHeader = document.createElement('h1');
-    titleHeader.textContent = 'Title:';
-    let authorHeader = document.createElement('h2');
-    authorHeader.textContent = 'Author:';
-    let pagesHeader = document.createElement('h2');
-    pagesHeader.textContent = 'Pages:';
-    let readHeader = document.createElement('h2');
-    readHeader.textContent = 'Status:';
+    //Add values to cells
+    titleCell.textContent = title;
+    authorCell.textContent = author;
+    pagesCell.textContent = pages;
 
-    //Book info HTML
-    let bookTitle = document.createElement('p');
-    bookTitle.textContent = title;
-    let bookAuthor = document.createElement('p');
-    bookAuthor.textContent = author;
-    let bookPages = document.createElement('p');
-    bookPages.textContent = pages;
-    let bookIsRead = document.createElement('p');
+    //Create label and checkbox element
+    let newLabel = document.createElement('LABEL');
+    let newCheckBox = document.createElement('INPUT');
+    newLabel.setAttribute('for', "statusCheckbox");
+    newCheckBox.setAttribute('type', 'checkbox');
+    newCheckBox.setAttribute('name', 'statusCheckbox');
+    newCheckBox.setAttribute('id', 'statusCheckbox');
+    newLabel.textContent = 'Completed';
+    newLabel.appendChild(newCheckBox);
+    statusCell.appendChild(newLabel);
 
     if (read == true) {
-        bookIsRead.textContent = 'Finished';
+        newCheckBox.checked = true;
     } else {
-        bookIsRead.textContent = 'Unfinished';
+        newCheckBox.checked = false;
     }
 
-    //Add all text elements to card
-    cardDiv.appendChild(titleHeader);
-    cardDiv.appendChild(bookTitle);
-    cardDiv.appendChild(authorHeader);
-    cardDiv.appendChild(bookAuthor);
-    cardDiv.appendChild(pagesHeader);
-    cardDiv.appendChild(bookPages);
-    cardDiv.appendChild(readHeader);
-    cardDiv.appendChild(bookIsRead);
-
-    //Buttons
-    let buttonDiv = document.createElement('div');
-    buttonDiv.className = 'container';
-    let statusButton = document.createElement('button');
-    statusButton.textContent = 'UPDATE';
-    statusButton.id = 'statusBtn';
-    let deleteButton = document.createElement('button');
-    deleteButton.textContent = 'DELETE';
-    deleteButton.id = 'deleteBtn';
-
-    //Delete card and splice library array
-    deleteButton.addEventListener('click', function() {
-        cardDiv.parentNode.removeChild(cardDiv)
-        library.splice(cardId, 1);
+    newCheckBox.addEventListener('change', function() {
+        if (this.checked) {
+            library[rowId].read = true;
+        } else {
+            library[rowId].read = false;
+        }
     });
 
+    let deleteButton = document.createElement('button');
+    deleteButton.textContent = 'REMOVE';
+    deleteButton.id = 'deleteBtn';
 
-    //Add card to body
-    buttonDiv.appendChild(statusButton);
-    buttonDiv.appendChild(deleteButton);
-    cardDiv.appendChild(buttonDiv)
-    body.appendChild(cardDiv);
+    //Delete row and splice library array
+    deleteButton.addEventListener('click', function() {
+        row.parentNode.removeChild(row);
+        library.splice(rowId, 1);
+    });
+
+    removeButtonCell.appendChild(deleteButton);
 }
 
 //HTML body
@@ -86,7 +71,7 @@ let body = document.querySelector('#cardContainer');
 let book1 = new Book('The Fellowship of the Ring', 'J.R.R Tolkien', 300, true, 0);
 let book2 = new Book('The Two Towers', 'J.R.R Tolkien', 400, true, 1);
 let book3 = new Book('The Return of the King', 'J.R.R Tolkien', 500, false, 2);
-let book4 = new Book('The Return of the Ting', 'J.R.R Tolkien', 500, false, 3);
+let book4 = new Book('The Return of the Ting', 'J.R.R Tolkien', 500, true, 3);
 let book5 = new Book('The Return of the Bling', 'J.R.R Tolkien', 500, false, 4);
 
 //Library array holding test books for debugging
@@ -97,6 +82,6 @@ function addBookToLibrary(title, author, pages, read) {
     library.push(new Book(title, author, pages, read))
 }
 
-// library.forEach(book => {
-//     createCard(book.title, book.author, book.pages, book.read, book.bookId)
-// });
+library.forEach(book => {
+    createRow(book.title, book.author, book.pages, book.read, book.bookId)
+});
